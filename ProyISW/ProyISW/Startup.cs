ec0fd843ee745212b929a8bb9feca710a6f04794
carfelip93 - Carlos Velozo <c.velozoweinberger@gmail.com>
2015-06-23 17:43:28 -0300

    Commit Inicial

---
+++ ProyISW/ProyISW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyISW.Startup))]
+namespace ProyISW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

64c7b42e7361d4d731aba1bbb1c7be06206b7bee
carfelip93 - Carlos Velozo <c.velozoweinberger@gmail.com>
2015-07-01 22:23:44 -0400

    prueba model commit

---
+++ ProyISW/ProyISW/Models/Prueba.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyISW.Models
+{
+    public class Prueba
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Cantidad { get; set; }
+        
+    }
+}
